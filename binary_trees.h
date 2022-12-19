e735dc7b5c90cccdcbbb6bb6ff2126feb2148698
OnyeEbube <okontaonyeka@gmail.com>
2022-12-19 13:55:29 +0000

    adding files

--- binary_trees.h
+++ binary_trees.h
@@ -78,5 +78,15 @@ void bal(avl_t **tree);
 avl_t *avl_remove(avl_t *root, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
 void sata_helper(avl_t **root, int *array, size_t lo, size_t hi);
-
+int binary_tree_is_heap(const binary_tree_t *tree);
+int btih_helper(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
+size_t tree_height(const heap_t *tree);
+size_t tree_size_h(const binary_tree_t *tree);
+void _preorder(heap_t *tree, heap_t **node, size_t height);
+void heapify(heap_t *root);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
+size_t tree_size(const binary_tree_t *tree);
 #endif
